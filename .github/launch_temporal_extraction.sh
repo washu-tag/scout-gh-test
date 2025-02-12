220dcb69f55974e3f6f9cca56766f3367310317e
Charlie Moore <moore.c@wustl.edu>
2025-02-12 14:16:50 -0600

    experimenting

---
+++ .github/launch_temporal_extraction.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+for logdate in $(find tests/staging_test_data/hl7 -name '*.log' | xargs -L 1 basename | cut -c1-8 | sort)
+do
+    echo "Sending date $logdate to temporal..."
+    kubectl exec -n temporal -it service/temporal-admintools -- temporal workflow start --task-queue ingest-hl7-log --type IngestHl7LogWorkflow --input '{"deltaLakePath":"s3://test/orchestration/delta/test_data", "hl7OutputPath":"s3://test/orchestration/hl7", "scratchSpaceRootPath":"s3://test/orchestration/scratch", "logsRootPath": "/hl7logs", "date": "'$logdate'"}';
+done
