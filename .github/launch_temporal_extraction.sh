9fd831feb494c411bcd25c20523b8c0f64754a17
Charlie Moore <moore.c@wustl.edu>
2025-02-13 16:37:34 -0600

    experimenting

--- .github/launch_temporal_extraction.sh
+++ .github/launch_temporal_extraction.sh
@@ -3,7 +3,7 @@
 for logdate in $(find tests/staging_test_data/hl7/1999 -name '*.log' | xargs -L 1 basename | cut -c1-8 | sort)
 do
     echo "Sending date $logdate to temporal..."
-    sudo kubectl exec -n temporal -i service/temporal-admintools -- temporal workflow start --task-queue ingest-hl7-log --type IngestHl7LogWorkflow --input '{"deltaLakePath":"s3://test/orchestration/delta/test_data", "hl7OutputPath":"s3://test/orchestration/hl7", "scratchSpaceRootPath":"s3://test/orchestration/scratch", "logsRootPath": "/hl7logs", "date": "'$logdate'"}';
+    sudo kubectl exec -n temporal -i service/temporal-admintools -- temporal workflow start --task-queue ingest-hl7-log --type IngestHl7LogWorkflow --input '{"deltaLakePath":"s3://lake/orchestration/delta/test_data", "hl7OutputPath":"s3://lake/orchestration/hl7", "scratchSpaceRootPath":"s3://lake/orchestration/scratch", "logsRootPath": "/hl7logs", "date": "'$logdate'"}';
 done
 
 max_wait=50
@@ -17,12 +17,11 @@ for ((i = 0; i <= max_wait; ++i)); do
 
     sleep 1s
     if [[ i -eq max_wait ]]; then
-        sudo kubectl -n orchestration-workers logs deployment/temporal-java
-        sudo kubectl -n minio logs minio-0
-        sudo kubectl -n minio logs minio-1
-        sudo kubectl get all -n minio-scout
-        sudo kubectl -n minio-scout describe service minio
-        sudo kubectl exec -n temporal -i service/temporal-admintools -- temporal workflow list -o json
+        echo "DEBUGGING:"
+        sudo kubectl describe pod minio-scout-pool-0-0 -n minio-scout
+        sudo kubectl logs minio-scout-pool-0-0 -n minio-scout
+        sudo kubectl get pv
+        sudo kubectl get pvc -n minio-scout
         sudo kubectl exec -n temporal -i service/temporal-admintools -- temporal workflow list -o json | jq -r '.[] | "\(.execution.workflowId) \(.execution.runId)"' | while read workflowId runId; do
             echo "Workflow id $workflowId and run id $runId"
             sudo kubectl exec -n temporal -i service/temporal-admintools -- temporal workflow show --workflow-id $workflowId --run-id $runId
