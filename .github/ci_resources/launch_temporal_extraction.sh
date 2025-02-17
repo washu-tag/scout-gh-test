7bc948082353c9e224eef6cc4e2d1f2aa37769f8
Charlie Moore <moore.c@wustl.edu>
2025-02-17 17:52:49 -0600

    Trying again

--- .github/ci_resources/launch_temporal_extraction.sh
+++ .github/ci_resources/launch_temporal_extraction.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 do_wait="wait"
-for logdate in $(find tests/staging_test_data/hl7 -name '*.log' | xargs -L 1 basename | cut -c1-8 | sort)
+for logdate in $(find tests/staging_test_data/hl7 -name '1994*.log' | xargs -L 1 basename | cut -c1-8 | sort)
 do
     echo "Sending date $logdate to temporal..."
     sudo kubectl exec -n temporal -i service/temporal-admintools -- temporal workflow start --task-queue ingest-hl7-log --type IngestHl7LogWorkflow --input '{"deltaLakePath":"s3://lake/orchestration/delta/test_data", "hl7OutputPath":"s3://lake/orchestration/hl7", "scratchSpaceRootPath":"s3://lake/orchestration/scratch", "logsRootPath": "/hl7logs", "date": "'$logdate'"}';
@@ -12,7 +12,7 @@ do
     fi
 done
 
-max_wait=60
+max_wait=30
 for ((i = 0; i <= max_wait; ++i)); do
     if sudo kubectl exec -n temporal -i service/temporal-admintools -- temporal workflow list -o json | jq '[.[] | select(.taskQueue == "ingest-hl7-log")] | all(.[]; .status == "WORKFLOW_EXECUTION_STATUS_COMPLETED")' -e > /dev/null; then
         echo "All workflows completed as expected"
