8289eb3d8ac577a4bd5eabb19eb0d98283fcff77
Charlie Moore <moore.c@wustl.edu>
2025-02-17 14:33:19 -0600

    experimenting

--- .github/launch_temporal_extraction.sh
+++ .github/launch_temporal_extraction.sh
@@ -5,7 +5,7 @@ for logdate in $(find tests/staging_test_data/hl7 -name '*.log' | xargs -L 1 bas
 do
     echo "Sending date $logdate to temporal..."
     sudo kubectl exec -n temporal -i service/temporal-admintools -- temporal workflow start --task-queue ingest-hl7-log --type IngestHl7LogWorkflow --input '{"deltaLakePath":"s3://lake/orchestration/delta/test_data", "hl7OutputPath":"s3://lake/orchestration/hl7", "scratchSpaceRootPath":"s3://lake/orchestration/scratch", "logsRootPath": "/hl7logs", "date": "'$logdate'"}';
-    if [[ do_wait -eq "wait" ]] then
+    if [[ "$do_wait" = "wait" ]] then
         echo "Trying to wait to check race condition..."
         sleep 30s
         do_wait="nope"
