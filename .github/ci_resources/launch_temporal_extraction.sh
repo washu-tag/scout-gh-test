154140ae892618f9ce913cb83130d2c033fbeffe
Kate Alpert <alpertk@wustl.edu>
2025-02-20 13:42:03 -0700

    SCOUT-50 Ansible enhancements and cleanup (#27)
    * Clone scout repo before installing services
    
    * Add support for multinode cluster
    
    * Change to not use custom domain with Github Pages
    
    Currently, the github page is vulnerable to takeover in supply-chain
    attack:
    https://docs.github.com/en/pages/configuring-a-custom-domain-for-your-github-pages-site/verifying-your-custom-domain-for-github-pages
    
    Evidence it is not verified:
    dig _github-pages-challenge-temporalio.temporal.io  +nostats +nocomments
    +nocmd TXT
    
    They seem to be verified with everyone else, but github ;-) :
    dig temporal.io  TXT
    
    * Download k3s installer to filesystem
    
    File artifacts are more idempotent than executing piped contents. Also
    makes troubleshooting easier, did the download fail? Then internet
    connectivity is the problem? Did the script execution fail? Then their
    code our variables are the problem.
    
    Also, in supply-chain attacks, it helps infosec forensics to have
    artifacts of what was actually run rather than running things from
    memory and having no artifact.
    
    * Use environment variables instead of flags
    
    When running the installer. Using variables in strings that will be
    executed, without proper sanitization lends itself to command injection
    attacks. By passing as environment variable, the variable is limited to
    being as string passed into the installer script. This attack 'can' is
    kicked down the road, did the authors of the installer sanitize
    correctly to protect attackees?
    
    ```bash
    # --- re-evaluate args to include env command ---
    eval set -- $(escape "${INSTALL_K3S_EXEC}") $(quote "$@")
    ```
    
    Yes!
    
    * Create master playbook to load other playbooks
    
    Using independent Make jobs means reading or running the inventory and
    password files.
    
    More idiomatic ansible to have a playbook of playbooks pattern.
    
    * Use Make variables instead of hardcoded values
    
    This will be especially useful for overriding the FQDN values, if
    developing playbooks for multiple machines on localhost.
    
    The VAULT_PASSWORD_ARG override variable will allow puppeteers (me) to
    use --ask-vault-password instead of being limited to
    --vault-password-file. Using a password file will probably require a
    ramfs mount to temporarily/securely host the vault, whereas -J can use
    expect to pass interactive passwords.
    
    * Install python's Kubernetes library earlier
    
    On a new "server" system, the package is required in the k3s.yaml task.
    Being installed so late results in k3s.yaml throwing this error:
    Failed to import the required Python library (kubernetes) on
    tag-big-05.nrg.wustl.edu's Python /usr/bin/python3
    
    Also, I changed it from a pip install to a system package install,
    otherwise, this has the hidden dependency of needing the package
    python3-pip installed first.
    
    * Download get-helm-3 and keep file around
    
    * Updates for GHA to work with new k3s install
    
    ---------
    
    Co-authored-by: Moises Baltazar <null@moisesb.com>

--- .github/ci_resources/launch_temporal_extraction.sh
+++ .github/ci_resources/launch_temporal_extraction.sh
@@ -2,25 +2,25 @@
 
 all_dates=$(find tests/staging_test_data/hl7 -name '*.log' | xargs -L 1 basename | cut -c1-8 | sort | paste -sd ",")
 echo "Submitting dates to temporal: $all_dates"
-sudo kubectl exec -n temporal -i service/temporal-admintools -- temporal workflow start --task-queue ingest-hl7-log --type IngestHl7LogWorkflow --input '{"deltaLakePath":"s3://lake/orchestration/delta/test_data", "hl7OutputPath":"s3://lake/orchestration/hl7", "scratchSpaceRootPath":"s3://lake/orchestration/scratch", "logsRootPath": "/hl7logs", "date": "'$all_dates'"}'
+kubectl exec -n temporal -i service/temporal-admintools -- temporal workflow start --task-queue ingest-hl7-log --type IngestHl7LogWorkflow --input '{"deltaLakePath":"s3://lake/orchestration/delta/test_data", "hl7OutputPath":"s3://lake/orchestration/hl7", "scratchSpaceRootPath":"s3://lake/orchestration/scratch", "logsRootPath": "/hl7logs", "date": "'$all_dates'"}'
 
 max_wait=300
 for ((i = 0; i <= max_wait; ++i)); do
-    if sudo kubectl exec -n temporal -i service/temporal-admintools -- temporal workflow list -o json | jq '[.[] | select(.taskQueue == "ingest-hl7-log")] | all(.[]; .status == "WORKFLOW_EXECUTION_STATUS_COMPLETED") and length > 0' -e > /dev/null; then
+    if kubectl exec -n temporal -i service/temporal-admintools -- temporal workflow list -o json | jq '[.[] | select(.taskQueue == "ingest-hl7-log")] | all(.[]; .status == "WORKFLOW_EXECUTION_STATUS_COMPLETED") and length > 0' -e > /dev/null; then
         echo "All workflows completed as expected"
-        sudo kubectl exec -n temporal -i service/temporal-admintools -- temporal workflow list -o json
+        kubectl exec -n temporal -i service/temporal-admintools -- temporal workflow list -o json
         exit 0
     else
         echo "Not all workflows completed, waiting and trying again..."
-        sudo kubectl exec -n temporal -i service/temporal-admintools -- temporal workflow list -o json
+        kubectl exec -n temporal -i service/temporal-admintools -- temporal workflow list -o json
     fi
 
     sleep 1s
     if [[ i -eq max_wait ]]; then
         echo "DEBUGGING:"
-        sudo kubectl exec -n temporal -i service/temporal-admintools -- temporal workflow list -o json | jq -r '.[] | "\(.execution.workflowId) \(.execution.runId)"' | while read workflowId runId; do
+        kubectl exec -n temporal -i service/temporal-admintools -- temporal workflow list -o json | jq -r '.[] | "\(.execution.workflowId) \(.execution.runId)"' | while read workflowId runId; do
             echo "Workflow id $workflowId and run id $runId"
-            sudo kubectl exec -n temporal -i service/temporal-admintools -- temporal workflow show --workflow-id $workflowId --run-id $runId
+            kubectl exec -n temporal -i service/temporal-admintools -- temporal workflow show --workflow-id $workflowId --run-id $runId
         done
         exit 25
     fi
