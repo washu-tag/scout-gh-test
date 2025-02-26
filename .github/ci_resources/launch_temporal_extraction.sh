d2d59ea797e8aeeb9bfb3d8bd76e0cdff4bfee24
Charlie Moore <moore.c@wustl.edu>
2025-02-26 15:53:23 -0600

    Experiment

--- .github/ci_resources/launch_temporal_extraction.sh
+++ .github/ci_resources/launch_temporal_extraction.sh
@@ -3,7 +3,7 @@
 max_wait=300
 for logdate in $(find tests/staging_test_data/hl7 -name '*.log' | xargs -L 1 basename | cut -c1-8 | sort); do
     echo "Submitting date to temporal: $logdate"
-    read workflow_id run_id < <(echo $(kubectl exec -n temporal -i service/temporal-admintools -- temporal workflow start --task-queue ingest-hl7-log --type IngestHl7LogWorkflow --input '{"deltaLakePath":"s3://lake/orchestration/delta/test_data", "hl7OutputPath":"s3://lake/orchestration/hl7", "scratchSpaceRootPath":"s3://lake/orchestration/scratch", "logsRootPath": "/hl7logs", "date": "'$logdate'"}' -o json | jq -r '.workflowId, .jobId'))
+    read workflow_id run_id < <(echo $(kubectl exec -n temporal -i service/temporal-admintools -- temporal workflow start --task-queue ingest-hl7-log --type IngestHl7LogWorkflow --input '{"deltaLakePath":"s3://lake/orchestration/delta/test_data", "hl7OutputPath":"s3://lake/orchestration/hl7", "scratchSpaceRootPath":"s3://lake/orchestration/scratch", "logsRootPath": "/hl7logs", "date": "'$logdate'"}' -o json | jq -r '.workflowId, .runId'))
     for ((i = 0; i <= max_wait; ++i)); do
         kubectl exec -n temporal -i service/temporal-admintools -- temporal workflow show --workflow-id $workflow_id --run-id $run_id
         break;
