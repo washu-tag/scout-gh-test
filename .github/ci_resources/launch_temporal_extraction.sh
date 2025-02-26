9aa56ef989cb249e9e368f4bb4d04380cf5c99d0
Charlie Moore <moore.c@wustl.edu>
2025-02-26 15:38:27 -0600

    Experiment

--- .github/ci_resources/launch_temporal_extraction.sh
+++ .github/ci_resources/launch_temporal_extraction.sh
@@ -1,29 +1,11 @@
 #!/bin/bash
 
-for logdate in $(find tests/staging_test_data/hl7 -name '*.log' | xargs -L 1 basename | cut -c1-8 | sort); do
-    echo "Submitting date to temporal: $all_dates"
-    kubectl exec -n temporal -i service/temporal-admintools -- temporal workflow start --task-queue ingest-hl7-log --type IngestHl7LogWorkflow --input '{"deltaLakePath":"s3://lake/orchestration/delta/test_data", "hl7OutputPath":"s3://lake/orchestration/hl7", "scratchSpaceRootPath":"s3://lake/orchestration/scratch", "logsRootPath": "/hl7logs", "date": "'$logdate'"}' -o json
-    exit 30
-done
-
 max_wait=300
-for ((i = 0; i <= max_wait; ++i)); do
-    if kubectl exec -n temporal -i service/temporal-admintools -- temporal workflow list -o json | jq '[.[] | select(.taskQueue == "ingest-hl7-log")] | all(.[]; .status == "WORKFLOW_EXECUTION_STATUS_COMPLETED") and length > 0' -e > /dev/null; then
-        echo "All workflows completed as expected"
-        kubectl exec -n temporal -i service/temporal-admintools -- temporal workflow list -o json
-        exit 0
-    else
-        echo "Not all workflows completed, waiting and trying again..."
-        kubectl exec -n temporal -i service/temporal-admintools -- temporal workflow list -o json
-    fi
-
-    sleep 1s
-    if [[ i -eq max_wait ]]; then
-        echo "DEBUGGING:"
-        kubectl exec -n temporal -i service/temporal-admintools -- temporal workflow list -o json | jq -r '.[] | "\(.execution.workflowId) \(.execution.runId)"' | while read workflowId runId; do
-            echo "Workflow id $workflowId and run id $runId"
-            kubectl exec -n temporal -i service/temporal-admintools -- temporal workflow show --workflow-id $workflowId --run-id $runId
-        done
-        exit 25
-    fi
+for logdate in $(find tests/staging_test_data/hl7 -name '*.log' | xargs -L 1 basename | cut -c1-8 | sort); do
+    echo "Submitting date to temporal: $logdate"
+    read workflow_id run_id < <(echo $(kubectl exec -n temporal -i service/temporal-admintools -- temporal workflow start --task-queue ingest-hl7-log --type IngestHl7LogWorkflow --input '{"deltaLakePath":"s3://lake/orchestration/delta/test_data", "hl7OutputPath":"s3://lake/orchestration/hl7", "scratchSpaceRootPath":"s3://lake/orchestration/scratch", "logsRootPath": "/hl7logs", "date": "'$logdate'"}' -o json | jq -r '.workflowId, .jobId'))
+    for ((i = 0; i <= max_wait; ++i)); do
+        kubectl exec -n temporal -i service/temporal-admintools -- temporal workflow show --workflow-id $workflow_id --run-id $run_id
+        break;
+    done
 done
